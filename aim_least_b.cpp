4fa4ad0ff169842bd4cae0b64ed4c942a25de593
ruoyi wang <jungege321@126.com>
2024-07-03 16:35:13 +0800

    n中币值钱币，凑target最小张数解法

---
+++ aim_least_b.cpp
@@ -0,0 +1,34 @@
+
+
+
+#include <iostream>
+#include<limites>
+
+using namespace std;
+
+long MAX_VALUE = numeric_limites<int64_t>::max();
+
+/**
+ * 有n种币值纸币，且每种币值张数不限
+ * 给定目标：target，寻找最小张数解
+ */
+
+//length为数组长度
+int64_t get_last_B(int* array, int idx, int length, int target){
+    if(idx==length) return MAX_VALUE;
+    int ans = MAX_VALUE;
+    for(int zhang=0; array[idx]*zhang<=target;zhang++){
+        int next = get_last_B(array, idx+1, length, target-array[idx]);
+        ans = min(ans, next+zhang);
+    }
+    return ans;
+}
+
+
+
+
+int main(int argc, char** argv){
+
+
+    return 0;
+}
