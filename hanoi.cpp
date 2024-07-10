8ac1bb7b548dd64c647d99af181f443890578464
ruoyi wang <jungege321@126.com>
2024-07-10 09:39:55 +0800

    修改参数名称

--- hanoi.cpp
+++ hanoi.cpp
@@ -1,7 +1,7 @@
 #include<iostream>
 using namespace std;
 void move(int n, string src , string dst){
-    cout<< n << "号盘从位置"<< src<<"移动到"<<dst<<endl;
+    cout<< n << "号盘从位置"<< src<<"移动到"<<dst<<endl;//移动过程
 }
 /**
  * n: n号盘子
@@ -11,18 +11,18 @@ void move(int n, string src , string dst){
  */
 void hanoi(int n, string src, string dst, string mid){
     if(n==1){
-        move(n, src, dst);
+        move(n, src, dst);//base case
         return;
     }else{
-        hanoi(n-1, src, min, dst);
+        hanoi(n-1, src, mid, dst);//将前n-1个盘子借助dst，放到mid
         move(n, src, dst);
-        hanoi(n-1, min, dst, src);
+        hanoi(n-1, mid, dst, src);//将前n-1个盘子借助src，放到dst
         return;
     }
 }
 
 void main(int argc, char** argv){
-    int n=1;
+    int n=3;
     hanoi(n,"左","中","右");
     return;
 }
